784016bf59fdba4c0397b5c29a512533d602a61a
Eddie Antonio Santos <easantos@ualberta.ca>
2016-05-17 12:20:23 -0600

    Use modules (ES6).

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var exampleApp = angular.module('exampleApp', []);
 
 exampleApp
-  .controller('CustomerController', function ($scope) {
-    $scope.customers = [
-      { name: 'Dave', city: 'Phoenix'},
-      { name: 'Napur', city: 'Denver' },
-      { name: 'Heedy', city: 'Dallas'}
-    ];
+  .controller({
+    CustomerController($scope) {
+      $scope.customers = [
+        { name: 'Dave', city: 'Phoenix'},
+        { name: 'Napur', city: 'Denver' },
+        { name: 'Heedy', city: 'Dallas'}
+      ];
+    }
   });
-
