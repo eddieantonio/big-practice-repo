3cff48bdc779fd83cc82a6d2f97b0e5fa2a154c7
Eddie Antonio Santos <easantos@ualberta.ca>
2016-05-17 13:57:07 -0600

    Split out all the files.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 var app = angular.module('exampleApp', ['ngRoute']);
 
+app.value('appName', 'Example App');
+
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
@@ -11,35 +13,4 @@ app.config(function ($routeProvider) {
       templateUrl: 'app/views/alternate.html'
     })
     .otherwise({ redirectTo: '/' });
-});
-
-app.factory('simpleFactory', function () {
-  var customers = [
-    { name: 'Dave', city: 'Phoenix'},
-    { name: 'Napur', city: 'Denver' },
-    { name: 'Heedy', city: 'Dallas'}
-  ];
-
-  return {
-    getCustomers() {
-      return customers;
-    }
-  };
-});
-
-app.controller('CustomerController', function ($scope, simpleFactory) {
-  $scope.customers = [];
-
-  init();
-
-  function init() {
-    $scope.customers = simpleFactory.getCustomers();
-  }
-
-  $scope.addCustomer = function() {
-    $scope.customers.push({
-      name: $scope.newCustomer.name,
-      city: $scope.newCustomer.city
-    });
-  };
 });
