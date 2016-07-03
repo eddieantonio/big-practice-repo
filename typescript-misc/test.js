65e6c18834350408653849bf998d8e1730141b26
Eddie Antonio Santos <easantos@ualberta.ca>
2016-07-03 15:11:51 -0600

    Fix makefile and test.

--- typescript-misc/test.js
+++ typescript-misc/test.js
@@ -1,16 +1,15 @@
 var assert = require('assert');
-if (0) {
-    var lfsr = require('./lfsr');
 
-    /* Ensures that the sequence produces at least 2^16 of random bits. */
-    var N = 65536;
-    var set = new Set();
-    for (var i = 0; i < N; i++) {
-        set.add(lfsr.random());
-    }
+var lfsr = require('./lfsr');
 
-    assert(set.size >= N, `${set.size} < ${N} Produced too few random numbers`);
-} else {
-    var offset = require('./offset').offset;
-    assert.equal(offset(), '-0600');
+/* Ensures that the sequence produces at least 2^16 of random bits. */
+var N = 65536;
+var set = new Set();
+for (var i = 0; i < N; i++) {
+  set.add(lfsr.random());
 }
+
+assert(set.size >= N, `${set.size} < ${N} Produced too few random numbers`);
+
+var offset = require('./offset').offset;
+assert.equal(offset(), '-0600');
