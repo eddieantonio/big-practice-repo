ddc747fd0feb6038dd64f90c448462998b82288e
Eddie Antonio Santos <easantos@ualberta.ca>
2017-06-13 20:54:19 -0600

    All of the usage stuff.

--- binary.c
+++ binary.c
@@ -1,24 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 #include <dlfcn.h>
 
+static const char *program_name;
+
+static void usage(FILE *output) {
+    fprintf(output, "Usage:\n");
+    fprintf(output, "  %s (add|mul) <i> <j>\n", program_name);
+}
+
+static void usage_error(const char *msg, ...) {
+    if (msg != NULL) {
+        fprintf(stderr, "%s: ", program_name);
+        va_list ap;
+        va_start(ap, msg);
+        vfprintf(stderr, msg, ap);
+        va_end(ap);
+        fprintf(stderr, "\n");
+    }
+    usage(stderr);
+    exit(2);
+}
+
+static int int_or_die(const char* str) {
+    char *end;
+    long answer = strtol(str, &end, 10);
+    if (str == end) {
+        usage_error("invalid integer: '%s'", str);
+    }
+    return answer;
+}
 
 int main(int argc, char **argv) {
+    program_name = argv[0];
     /* TODO: accept three arguments like subcommands. */
     if (argc != 4) {
-        return 2;
+        usage_error("requires exactly 3 arguments");
     }
-    int a = atoi(argv[2]), b = atoi(argv[3]);
+    int a = int_or_die(argv[2]), b = int_or_die(argv[3]);
     const char *library;
 
-    if (strncmp(argv[1], "add", 3) == 0) {
+    if (strncmp(argv[1], "add", 4) == 0) {
         library = "binary_add.dylib";
-    } else if (strncmp(argv[1], "mul", 3) == 0) {
+    } else if (strncmp(argv[1], "mul", 4) == 0) {
         library = "binary_mul.dylib";
     } else {
-        return 2;
+        usage_error("invalid subcommand: '%s'", argv[1]);
     }
 
     void *lib = dlopen(library, RTLD_LAZY);
